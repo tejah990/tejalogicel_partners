438d6d166578958e889e7a940ca7988c7313065a
krivena <codexidphone@gmail.com>
2025-04-30 14:07:07 +0530

    Update

--- main.tf
+++ main.tf
@@ -18,3 +18,22 @@ module "network-budget" {
   network_budget_name     =    var.network_budget_name
   network_limit_amount       = var.network_limit_amount
 }
+
+resource "aws_budgets_budget" "ec2_usage_budget" {
+  name              = "ec2-monthly-usage-budget"
+  budget_type       = "USAGE"
+  limit_amount      = "0.20"                    # e.g. max 720 hours per month
+  limit_unit        = "HOURS"
+  time_unit         = "DAILY"
+
+
+
+  notification {
+    comparison_operator = "GREATER_THAN"
+    threshold           = 30
+    threshold_type      = "PERCENTAGE"
+    notification_type   = "ACTUAL"
+    subscriber_email_addresses  = ["phonexid65@gmail.com"]
+  }
+  # Removed cost_filters block because it's not valid for USAGE budgets.
+}
